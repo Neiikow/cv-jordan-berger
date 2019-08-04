900ba2151ccf1827f938379df8627e074d4a0926
Neiikow <bergerjordan24@gmail.com>
2019-08-04 12:51:52 +0200

    Front : Positionnement header et contenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageAdminComponent } from './components/pages/page-admin/page-admin.component';
 import { PageCvComponent } from './components/pages/page-cv/page-cv.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageAdminComponent,
     PageCvComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
